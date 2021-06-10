5c2672e872653877670a324330e79d79fa695e7c
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-10 10:03:06 -0300

    Adicionei a solução do sexto desafio do PSEL

---
+++ Tem_copa_Tem_jogo_Nao_MEH.js
@@ -0,0 +1,12 @@
+function solucao(ano) {
+	//seu codigo aqui
+    if(ano === 2022 || ano % 4 === 2) {
+       console.log("COPA")
+       } else {
+           if(ano === 2024 || ano % 4 === 0) {
+              console.log("JOGOS")
+              } else {
+                  console.log("MEH")
+              }
+       }
+}
